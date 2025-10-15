0a64d150622b3c45dd47575d904f63a6e2ab1a14
BountyHunterOka <434291091spd@gmail.com>
2025-10-15 20:05:29 +0800

    Update 多陪server.py

--- 多陪server.py
+++ 多陪server.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -31,7 +31,6 @@ def start_book():
     thread = threading.Thread(target=start_grabbing, daemon=True)
     thread.start()
     return "开始抢单"
-
 
 @app.get("/stop")
 def stop_book():
@@ -163,69 +162,39 @@ def extract_order_id(decrypted_json_str):
         log(f"[提取订单 ID 失败] {e}")
     return None
 
-def extract_ts(decrypted_json_str):
-    try:
-        data = json.loads(decrypted_json_str)
-        li = data.get("list", [])
-        timestamp = li[0].get("createTime") / 1000
-        print(timestamp)
-
-        return timestamp
-    except Exception as e:
-        print(f"[提取订单 ts 失败] {e}")
-    return None
-
-
-def sleep_time(create_ts, wait_time):
-    now_ts = datetime.now(tz).timestamp()
-    target_ts = create_ts + wait_time + random.uniform(3.7, 6)
-    sleep_seconds = target_ts - now_ts
-    if sleep_seconds < 0:
-        random_sleep = random.uniform(1, 2.5)
-    else:
-        random_sleep = random.uniform(sleep_seconds, sleep_seconds+2.25)
-    print(sleep_seconds)
-    time.sleep(random_sleep)
-# ========== 抢单 ==========
-def confirm_order(order_id,create_ts):
+def confirm_order(order_id):
     url = f"{BASE_URL}/s/c/order/confirm"
     data = {"id": order_id}
     try:
-        sleep_time(create_ts, 15.3)
         while running:
+            time.sleep(7)
             resp = session.post(url, data=data, timeout=3.5)
             da = resp.json()
             confirm_rep = decrypt_aes_cbc(da["response"], KEY_HEX, IV_HEX)
             if not confirm_rep:
                 break
             log(f"[抢单结果] {confirm_rep}")
-            # if '未满足' in confirm_rep:
-            #     log("等待中...继续尝试")
-            #     time.sleep(9.5)
-            #     continue
             break
     except Exception as e:
         log(f"[抢单失败] {e}")
 
-# ========== 主循环 ==========
 def run_loop(interval):
     global running
     while running:
-        now = datetime.now(tz)
         print("刷新时间 =", datetime.now(tz))
         decrypted = refresh_list()
         if decrypted:
             order_id = extract_order_id(decrypted)
             if order_id:
-                create_ts = extract_ts(decrypted)
+                # create_ts = extract_ts(decrypted)
                 log(f"[发现订单] ID = {order_id}")
-                confirm_order(order_id, create_ts)
+                confirm_order(order_id)
                 # play_sound()
             else:
                 log("[无新订单]")
         else:
             log("[解密失败或网络异常]")
-        time.sleep(random.uniform(2, 3.5))
+        time.sleep(random.uniform(1, 2))
 
 # ========== 控制函数 ==========
 def start_grabbing():
